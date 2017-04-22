0f013c0aa657d795e491c2c5c9a789c0efd3bd97
sigma425 <t.sigma425@gmail.com>
2017-04-22 20:47:35 +0900

    fix segtree ( D::e -> D::e() )

--- data structure/segtree/segtree_simple.cpp
+++ data structure/segtree/segtree_simple.cpp
@@ -1,8 +1,5 @@
 /*
-	point assign,range +
-	書くものは
-	D op()	(演算)
-	D e		(単位元)
+	point assign,range + 非再帰
 */
 #include <bits/stdc++.h>
 #define rep(i,n) for(int i=0;i<n;i++)
@@ -20,13 +17,13 @@ struct segtree_simple{
 	segtree_simple(int n){
 		N=1;
 		while(N<n) N*=2;
-		val.assign(N*2,D::e);
+		val.assign(N*2,D::e());
 	}
 	segtree_simple(const vector<D>& ds){
 		int n = ds.size();
 		N=1;
 		while(N<n) N*=2;
-		val.assign(N*2,D::e);
+		val.assign(N*2,D::e());
 		rep(i,n) val[i+N] = ds[i];
 		for(int i=N-1;i>0;i--) val[i] = val[i*2] + val[i*2+1];
 	}
@@ -40,7 +37,7 @@ struct segtree_simple{
 		}
 	}
 	D query(int a,int b){		//non-commutative & unrecursive
-		D L = D::e , R = D::e;
+		D L = D::e() , R = D::e();
 		a+=N,b+=N;
 		while(a<b){
 			if(a&1) L = L + val[a++];
@@ -61,67 +58,99 @@ struct segtree_simple{
 
 struct Dplus{		//(int,+,0)
 	int x;
-	Dplus(){*this = e;}
+	Dplus(){}
 	Dplus(int x):x(x){}
-	const static Dplus e;
+	static Dplus e(){
+		return Dplus(0);
+	}
 	Dplus operator+(const Dplus& r) const {
 		return Dplus(x+r.x);
 	}
 	friend ostream& operator<<(ostream& o,const Dplus& d){return o<<d.x;}
 };
-const Dplus Dplus::e = Dplus(0);
 
 struct Dmax{		//(int,max,-inf)
 	int x;
-	Dmax(){*this = e;}
+	Dmax(){}
 	Dmax(int x):x(x){}
-	const static Dmax e;
+	static Dmax e(){
+		return Dmax(-1e9);
+	}
 	Dmax operator+(const Dmax& r) const {
 		return Dmax(max(x,r.x));
 	}
 	friend ostream& operator<<(ostream& o,const Dmax& d){return o<<d.x;}
 };
-const Dmax Dmax::e = Dmax(-1e9);
 
 struct Dmin{		//(int,min,inf)
 	int x;
-	Dmin(){*this = e;}
+	Dmin(){}
 	Dmin(int x):x(x){}
-	const static Dmin e;
+	static Dmin e(){
+		return Dmin(1e9);
+	}
 	Dmin operator+(const Dmin& r) const {
 		return Dmin(min(x,r.x));
 	}
 	friend ostream& operator<<(ostream& o,const Dmin& d){return o<<d.x;}
 };
-const Dmin Dmin::e = Dmin(1e9);
 
 struct Dleftmostmax{		//(int,max,-inf)
+	using D = Dleftmostmax;
 	int x,a;	//val,argid
-	Dleftmostmax(){*this = e;}
+	Dleftmostmax(){}
 	Dleftmostmax(int x,int a):x(x),a(a){}
-	const static Dleftmostmax e;
-	Dleftmostmax operator+(const Dleftmostmax& r) const {
+	static D e(){
+		return D(-1e9,-1);
+	}
+	D operator+(const D& r) const {
 		if(x>r.x || (x==r.x&&a<r.a)) return *this;
 		else return r;
 	}
-	friend ostream& operator<<(ostream& o,const Dleftmostmax& d){return o<<d.x;}
+	friend ostream& operator<<(ostream& o,const D& d){return o<<"("<<d.x<<","<<d.a<<")";}
 };
-const Dleftmostmax Dleftmostmax::e = Dleftmostmax(-1e9,-1);
 
 struct Dperm{
+	using D = Dperm;
 	const static int N = 5;
 	using V = array<int,N>;
 	V v;
-	Dperm(){*this = e;}
+	Dperm(){}
 	Dperm(V v):v(v){}
-	const static Dperm e;
-	Dperm operator+(const Dperm& r) const {
+	const static D e(){
+		return D({0,1,2,3,4});
+	}
+	D operator+(const D& r) const {
 		V ret;
 		rep(i,N) ret[i] = v[r.v[i]];
-		return Dperm(ret);
+		return D(ret);
 	};
 };
-const Dperm Dperm::e = Dperm({0,1,2,3,4});
+
+struct Dmat{
+	using T = int;
+	using D = Dmat;
+	const static int N = 5;
+	using V = array<T,N>;
+	using VV = array<V,N>;
+	VV a;
+	Dmat(){}
+	Dmat(VV a):a(a){}
+	const static D e(){
+		VV a;
+		rep(i,N) rep(j,N) a[i][j] = (i==j?1:0);
+		return D(a);
+	}
+	D operator+(const D& r) const {
+		VV c;
+		rep(i,N) rep(j,N) c[i][j]=0;
+		rep(i,N) rep(j,N) rep(k,N) c[i][j] += a[i][k] * r.a[k][j];
+		return D(c);
+	};
+	bool operator==(const D& r) const {
+		return a==r.a;
+	}
+};
 
 void unittest(){	//どっちかというとstruct Dのテスト
 	{
@@ -137,7 +166,7 @@ void unittest(){	//どっちかというとstruct Dのテスト
 		vector<Dmax> vs = {-4,2,-5,-4,8,-3,1,-4,1,5};
 		segtree_simple<Dmax> seg1(vs);
 		rep(l,10) for(int r=l;r<=10;r++){
-			int mx = Dmax::e.x;
+			int mx = Dmax::e().x;
 			for(int i=l;i<r;i++) chmax(mx,vs[i].x);
 			assert( seg1.query(l,r).x == mx );
 		}
@@ -149,7 +178,7 @@ void unittest(){	//どっちかというとstruct Dのテスト
 		rep(i,10) vs.push_back(D(vi[i],i));
 		segtree_simple<D> seg(vs);
 		rep(l,10) for(int r=l+1;r<=10;r++){
-			int mx = D::e.x;
+			int mx = D::e().x;
 			int ag = -1;
 			for(int i=l;i<r;i++){
 				if(mx<vi[i]){
@@ -165,7 +194,7 @@ void unittest(){	//どっちかというとstruct Dのテスト
 		vector<D> vs = {D({0,3,2,4,1}),D({0,3,2,1,4}),D({3,2,0,4,1}),D({0,3,4,2,1}),D({0,3,2,4,1}),D({0,3,1,4,2}),D({4,2,3,0,1}),D({3,2,4,1,0})};
 		segtree_simple<D> seg(vs);
 		int N = vs.size();
-		rep(l,N) for(int r=l+1;r<=N;r++){
+		rep(l,N) for(int r=l;r<=N;r++){
 			int a[5]={0,1,2,3,4};
 			for(int i=l;i<r;i++){
 				int na[5];
@@ -174,6 +203,27 @@ void unittest(){	//どっちかというとstruct Dのテスト
 			}
 			D calc = seg.query(l,r);
 			rep(i,5) assert( calc.v[i] == a[i] );
+		}
+	}
+	{
+		using D = Dmat;
+		const int K = Dmat::N;
+		using T = int;
+		using V = array<T,K>;
+		using VV = array<V,K>;
+
+		vector<D> vs;
+		int N = 10;
+		rep(i,N){
+			VV a;
+			rep(x,K) rep(y,K) a[x][y] = rand()%201+100;
+			vs.push_back(D(a));
+		}
+		segtree_simple<D> seg(vs);
+		rep(l,N) for(int r=l;r<=N;r++){
+			D x = D::e();
+			for(int i=l;i<r;i++) x = x+vs[i];
+			assert( seg.query(l,r) == x );
 		}
 	}
 }
