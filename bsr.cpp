7457497ca4205dd0eb1dae6e7e613c99d8548349
sigma425 <t.sigma425@gmail.com>
2017-04-25 18:51:41 +0900

    add Timer

--- bsr.cpp
+++ bsr.cpp
@@ -3,7 +3,11 @@ x       0  1  2  3  4  5  6  7  8  9
 bsr(x) -1  0  1  1  2  2  2  2  3  3
 最上位bit
 */
-int bsr(int x){
+int bsr(uint x){
 	if(x==0) return -1;
-	return 31 - __builtin_clz(x);
+	return 31 ^ __builtin_clz(x);
+}
+int bsr(unsigned long long x){
+	if(x==0) return -1;
+	return 63 ^ __builtin_clzll(x);
 }
---
+++ data structure/fastset.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+#define rep1(i,n) for(int i=1;i<=(int)(n);i++)
+#define all(c) c.begin(),c.end()
+#define pb push_back
+#define fs first
+#define sc second
+#define show(x) cout << #x << " = " << x << endl
+#define chmin(x,y) x=min(x,y)
+#define chmax(x,y) x=max(x,y)
+using namespace std;
+template<class S,class T> ostream& operator<<(ostream& o,const pair<S,T> &p){return o<<"("<<p.fs<<","<<p.sc<<")";}
+template<class T> ostream& operator<<(ostream& o,const vector<T> &vc){o<<"sz = "<<vc.size()<<endl<<"[";for(const T& v:vc) o<<v<<",";o<<"]";return o;}
+
+struct FastSet{	//[0,N)
+	using ull = unsigned long long;
+	int N,D;
+	vector<vector<ull>> seg;
+	FastSet(int N):N(N){
+		while(N>1){
+			N = (N+63)>>6;
+			seg.pb(vector<ull>(N));
+		}
+		D = seg.size();
+	}
+	void insert(int x){
+		rep(d,D){
+			ull b = 1ull<<(x&63);
+			x >>= 6;
+			seg[d][x] |= b;
+		}
+	}
+	void erase(int x){
+		rep(d,D){
+			ull b = 1ull<<(x&63);
+			x >>= 6;
+			seg[d][x] &= ~b;
+			if(seg[d][x]) break;
+		}
+	}
+	int geq(int x){		//x<=res
+		rep(d,D){
+			int i = x>>6, r = x&63;
+			if(i == (int)seg[d].size()) break;
+			ull b = seg[d][i]>>r;
+			if(b){
+				x += bsf(b);
+				while(d--){
+					x = (x<<6)|bsf(seg[d][x]);
+				}
+				return x;
+			}else{
+				x = (x>>6)+1;
+			}
+		}
+		return N;
+	}
+	int leq(int x){		//res<=x
+		rep(d,D){
+			if(x == -1) break;
+			int i = x>>6, r = x&63;
+			ull b = seg[d][i]<<(r^63);
+			if(b){
+				x -= bsr(b)^63;
+				while(d--){
+					x = (x<<6)|bsr(seg[d][x]);
+				}
+				return x;
+			}
+			x = (x>>6)-1;
+		}
+		return -1;
+	}
+
+	int bsr(ull x){return __builtin_clzll(x)^63;}
+	int bsf(ull x){return __bultin_ctzll(x);}
+
+};
+int main(){
+	FastSet<
+}
