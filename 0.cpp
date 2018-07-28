170c470fd2437d867262b2c4d2f1168feef8494b
sigma425 <sigma425@gmail.com>
2018-07-29 00:15:53 +0900

    add template

---
+++ 0.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+#define rep1(i,n) for(int i=1;i<=(int)(n);i++)
+#define all(c) c.begin(),c.end()
+#define pb push_back
+#define fs first
+#define sc second
+#define show(x) cout << #x << " = " << (x) << endl
+#define chmin(x,y) x=min(x,y)
+#define chmax(x,y) x=max(x,y)
+using namespace std;
+template<class S,class T> ostream& operator<<(ostream& o,const pair<S,T> &p){
+	return o<<"("<<p.fs<<","<<p.sc<<")";
+}
+template<class T> ostream& operator<<(ostream& o,const vector<T> &vc){
+	o<<"{";
+	for(const T& v:vc) o<<v<<",";
+	o<<"}";
+	return o;
+}
+using ll = long long;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+int main(){
+}
