472e08290ee354557294e1727ca217dadb858af9
sigma425 <sigma425@gmail.com>
2018-09-18 03:17:25 +0900

    renameのつもりだったけどミスってた

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 # library
 バグっても責任は取りません。
 
-gitのメモ
-
-git add .
-git commit -m "comment"
-git push
+ここにリストを入れる
--- string(文字列)/trie.cpp
+++
@@ -1,9 +0,0 @@
-string s;
-rep(i,s.size()){
-	int x=s[i]-'a';
-	if(d[v][x]==-1){
-		d[v][x]=d.size();
-		d.pb(vi(26,-1));
-	}
-	v=d[v][x];
-}
