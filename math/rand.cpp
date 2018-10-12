6eb6dac8a1bc5e0b533898acba77ffc0be574ee1
sigma425 <sigma425@gmail.com>
2018-10-12 13:58:13 +0900

    small fix

--- math/rand.cpp
+++ math/rand.cpp
@@ -1,11 +1,16 @@
+/*
+	rand
+	Tは整数型ならなんでもいい
+	実数ならuniform_real_distirbution
+*/
 template<class T>
-T rand_int(T l,T r){	//[l,r)
+T rnd(T l,T r){	//[l,r)
 	using D = uniform_int_distribution<T>;
 	static random_device rd;
 	static mt19937 gen(rd());
 	return D(l,r-1)(gen);
 }
 template<class T>
-ll rand_int(ll n){	//[0,n)
-	return rand_int(0,n-1);
+T rnd(T n){	//[0,n)
+	return rnd(0,n);
 }
