7e2cd206f0392a6ad6e17728db8290b7de47f53d
sigma425 <sigma425@gmail.com>
2018-10-12 14:17:55 +0900

    fix inv(binom)

--- math/inv.cpp
+++ math/inv.cpp
@@ -1,31 +1,20 @@
-const int MX=;
-ll f[MX+1],g[MX+1],inv[MX+1];
-void precalc(){
-	f[0]=1;
-	rep1(i,MX) f[i]=f[i-1]*i%mod;
-	inv[1]=1;
-	for(int i=2;i<=MX;i++) inv[i]=mod-mod/i*inv[mod%i]%mod;
-	g[0]=1;
-	rep1(i,MX) g[i]=g[i-1]*inv[i]%mod;
-}
-ll C(int x,int y){
-	if(y<0 || x<y) return 0;
-	return f[x]*g[y]%mod*g[x-y]%mod;
-}
-
-mint f[MX],g[MX],inv[MX];
-void precalc(){
-	f[0]=1;
-	rep1(i,MX-1) f[i] = f[i-1] * i;
+vector<mint> fac,ifac,inv;
+void precalc(int N){	//[0,N]
+	N++;
+	fac.resize(N);
+	ifac.resize(N);
+	inv.resize(N);
+	fac[0]=1;
+	rep1(i,N-1) fac[i] = fac[i-1] * i;
 
 	const int mod = 1e9+7;
 	inv[1]=1;
-	for(int i=2;i<=MX-1;i++) inv[i] = -inv[mod%i] * (mod/i);
+	for(int i=2;i<=N-1;i++) inv[i] = -inv[mod%i] * (mod/i);
 
-	g[0]=1;
-	rep1(i,MX-1) g[i] = g[i-1] *inv[i];
+	ifac[0]=1;
+	rep1(i,N-1) ifac[i] = ifac[i-1] *inv[i];
 }
-mint C(int x,int y){
+mint Binom(int x,int y){
 	if(y<0 || x<y) return 0;
-	return f[x]*g[y]*g[x-y];
+	return fac[x] * ifac[y] * ifac[x-y];
 }
