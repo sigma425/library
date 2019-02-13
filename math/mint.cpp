364b3fa265c755287797a74226017cbbfd5b910e
sigma425 <sigma425@gmail.com>
2019-02-13 23:01:45 +0900

    add pow,++,-- and avoid shadowing

--- math/mint.cpp
+++ math/mint.cpp
@@ -1,3 +1,9 @@
+/*
+	任意mod なら 
+	template なくして costexpr の行消して global に unsigned int mod = 1;
+	で cin>>mod してから使う
+*/
+
 template<unsigned int mod_>
 struct ModInt{
 	using uint = unsigned int;
@@ -8,7 +14,7 @@ struct ModInt{
 
 	uint v;
 	ModInt():v(0){}
-	ModInt(ll v):v(normS(v%mod+mod)){}
+	ModInt(ll _v):v(normS(_v%mod+mod)){}
 	explicit operator bool() const {return v!=0;}
 	static uint normS(const uint &x){return (x<mod)?x:x-mod;}		// [0 , 2*mod-1] -> [0 , mod-1]
 	static ModInt make(const uint &x){ModInt m; m.v=x; return m;}
@@ -21,20 +27,32 @@ struct ModInt{
 	ModInt& operator-=(const ModInt& b){ return *this=*this-b;}
 	ModInt& operator*=(const ModInt& b){ return *this=*this*b;}
 	ModInt& operator/=(const ModInt& b){ return *this=*this/b;}
+	ModInt& operator++(int){ return *this=*this+1;}
+	ModInt& operator--(int){ return *this=*this-1;}
 	ll extgcd(ll a,ll b,ll &x,ll &y) const{
-		ll u[]={a,1,0},v[]={b,0,1};
-		while(*v){
-			ll t=*u/ *v;
-			rep(i,3) swap(u[i]-=t*v[i],v[i]);
+		ll p[]={a,1,0},q[]={b,0,1};
+		while(*q){
+			ll t=*p/ *q;
+			rep(i,3) swap(p[i]-=t*q[i],q[i]);
 		}
-		if(u[0]<0) rep(i,3) u[i]=-u[i];
-		x=u[1],y=u[2];
-		return u[0];
+		if(p[0]<0) rep(i,3) p[i]=-p[i];
+		x=p[1],y=p[2];
+		return p[0];
 	}
-	ModInt inv() const{
+	ModInt inv() const {
 		ll x,y;
 		extgcd(v,mod,x,y);
 		return make(normS(x+mod));
+	}
+	ModInt pow(ll p) const {
+		ModInt a = 1;
+		ModInt x = *this;
+		while(p){
+			if(p&1) a *= x;
+			x *= x;
+			p >>= 1;
+		}
+		return a;
 	}
 	bool operator==(const ModInt& b) const { return v==b.v;}
 	bool operator!=(const ModInt& b) const { return v!=b.v;}
@@ -47,57 +65,3 @@ struct ModInt{
 	friend ostream& operator<<(ostream &o,const ModInt& x){ return o<<x.v;}
 };
 using mint = ModInt<1000000007>;
-
-/*
-	mod 可変版
-	実は素数じゃなくても互いに素でないやつの割り算以外の演算には使える
-*/
-
-unsigned int mod = 1;
-
-struct ModInt{
-	using uint = unsigned int;
-	using ll = long long;
-	using ull = unsigned long long;
-
-	uint v;
-	ModInt():v(0){}
-	ModInt(ll v):v(normS(v%mod+mod)){}
-	explicit operator bool() const {return v!=0;}
-	static uint normS(const uint &x){return (x<mod)?x:x-mod;}		// [0 , 2*mod-1] -> [0 , mod-1]
-	static ModInt make(const uint &x){ModInt m; m.v=x; return m;}
-	ModInt operator+(const ModInt& b) const { return make(normS(v+b.v));}
-	ModInt operator-(const ModInt& b) const { return make(normS(v+mod-b.v));}
-	ModInt operator-() const { return make(normS(mod-v)); }
-	ModInt operator*(const ModInt& b) const { return make((ull)v*b.v%mod);}
-	ModInt operator/(const ModInt& b) const { return *this*b.inv();}
-	ModInt& operator+=(const ModInt& b){ return *this=*this+b;}
-	ModInt& operator-=(const ModInt& b){ return *this=*this-b;}
-	ModInt& operator*=(const ModInt& b){ return *this=*this*b;}
-	ModInt& operator/=(const ModInt& b){ return *this=*this/b;}
-	ll extgcd(ll a,ll b,ll &x,ll &y) const{
-		ll u[]={a,1,0},v[]={b,0,1};
-		while(*v){
-			ll t=*u/ *v;
-			rep(i,3) swap(u[i]-=t*v[i],v[i]);
-		}
-		if(u[0]<0) rep(i,3) u[i]=-u[i];
-		x=u[1],y=u[2];
-		return u[0];
-	}
-	ModInt inv() const{
-		ll x,y;
-		extgcd(v,mod,x,y);
-		return make(normS(x+mod));
-	}
-	bool operator==(const ModInt& b) const { return v==b.v;}
-	bool operator!=(const ModInt& b) const { return v!=b.v;}
-	friend istream& operator>>(istream &o,ModInt& x){
-		ll tmp;
-		o>>tmp;
-		x=ModInt(tmp);
-		return o;
-	}
-	friend ostream& operator<<(ostream &o,const ModInt& x){ return o<<x.v;}
-};
-using mint = ModInt;
