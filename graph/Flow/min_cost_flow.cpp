065d3f18c1e9335a4d4ceed126bc2a2eabe18557
sigma425 <t.sigma425@gmail.com>
2019-03-14 23:15:46 +0900

    Fix MinCostFlow

--- graph/Flow/min_cost_flow.cpp
+++ graph/Flow/min_cost_flow.cpp
@@ -1,52 +1,75 @@
-typedef pair<int,int> P;
-struct edge {int to,cap,cost,rev;};
-const int MAX_V=,INF=;
-int V;			//代入!!
-vector<edge> G[MAX_V];
-int h[MAX_V];
-int dist[MAX_V];
-int prevv[MAX_V],preve[MAX_V];
-void add_edge(int from, int to, int cap, int cost){
-	edge e1={to,cap,cost,G[to].size()},e2={from,0,-cost,G[from].size()};
-	G[from].push_back(e1);
-	G[to].push_back(e2);
-}
-int min_cost_flow(int s, int t, int f){
-	int res=0;
-	fill(h,h+V,0);
-	while(f>0){
-		priority_queue< P,vector<P>,greater<P> > que;
-		fill(dist,dist+V,INF);
-		dist[s]=0;
-		que.push(P(0,s));
-		while(!que.empty()){
-			P p=que.top();
-			que.pop();
-			int v=p.second;
-			if(dist[v]<p.first) continue;
-			for(int i=0;i<G[v].size();i++){
-				edge &e=G[v][i];
-				if(e.cap>0 && dist[e.to]>dist[v]+e.cost+h[v]-h[e.to]){
-					dist[e.to]=dist[v]+e.cost+h[v]-h[e.to];
-					prevv[e.to]=v;
-					preve[e.to]=i;
-					que.push(P(dist[e.to],e.to));
+struct MinCostFlow{
+	using C = ll;		// capacity
+	using D = ll;		// cost (distance)
+	const D inf = 1e18;	// max distance
+
+	struct edge{
+		int to;
+		C cap;
+		D cost;
+		int rev;
+		edge(int to_,C cap_, D cost_, int rev_):to(to_),cap(cap_),cost(cost_),rev(rev_){}
+	};
+	
+	int N;
+	VV<edge> G;
+	V<D> h;
+	V<D> dist;
+	V<int> prevv,preve;
+	MinCostFlow(int N_):N(N_){
+		G.resize(N);
+		h.resize(N);
+		dist.resize(N);
+		prevv.resize(N);
+		preve.resize(N);
+	}
+
+	void add_edge(int from, int to, C cap, D cost){
+		show(cap);
+		show(cost);
+		edge e1(to,cap,cost,(int)G[to].size());
+		edge e2(from,0,-cost,(int)G[from].size());
+		G[from].push_back(e1);
+		G[to].push_back(e2);
+	}
+	D min_cost_flow(int s, int t, C f){
+		D res = 0;
+		h = V<D>(N);
+		while(f > 0){
+			using P = pair<D,int>;
+			priority_queue< P,vector<P>,greater<P> > que;
+			dist = V<D>(N,inf);
+			dist[s] = 0;
+			que.push(P(0,s));
+			while(!que.empty()){
+				P p = que.top();
+				que.pop();
+				int v = p.second;
+				if(dist[v] < p.first) continue;
+				for(int i=0;i<(int)G[v].size();i++){
+					edge &e = G[v][i];
+					if(e.cap>0 && dist[e.to]>dist[v]+e.cost+h[v]-h[e.to]){
+						dist[e.to]=dist[v]+e.cost+h[v]-h[e.to];
+						prevv[e.to]=v;
+						preve[e.to]=i;
+						que.push(P(dist[e.to],e.to));
+					}
 				}
 			}
+			if(dist[t]==inf) return -1;
+			rep(v,N) h[v]+=dist[v];
+			C d = f;
+			for(int v=t;v!=s;v=prevv[v]){
+				chmin(d,G[prevv[v]][preve[v]].cap);
+			}
+			f -= d;
+			res += d*h[t];
+			for(int v=t;v!=s;v=prevv[v]){
+				edge &e=G[prevv[v]][preve[v]];
+				e.cap-=d;
+				G[v][e.rev].cap+=d;
+			}
 		}
-		if(dist[t]==INF) return -1;
-		for(int v=0;v<V;v++) h[v]+=dist[v];
-		int d=f;
-		for(int v=t;v!=s;v=prevv[v]){
-			d=min(d,G[prevv[v]][preve[v]].cap);
-		}
-		f-=d;
-		res+=d*h[t];
-		for(int v=t;v!=s;v=prevv[v]){
-			edge &e=G[prevv[v]][preve[v]];
-			e.cap-=d;
-			G[v][e.rev].cap+=d;
-		}
+		return res;
 	}
-	return res;
-}
+};
