8a9fd85ea3743cd5229b85f7cae85a077bd636be
sigma425 <t.sigma425@gmail.com>
2019-06-01 20:53:51 +0900

    add judge/sample

---
+++ judge.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+g++ -std=c++17 -O3 -Wall -Wshadow -Wextra -D_GLIBCZZ_DEBUG -fsanitize=undefined,address $1.cpp -o $1
+for f in tests/*$1*.in; do
+    echo '#### Start ' $f
+    of=${f%.in}.out
+    af=${f%.in}.ans
+    ./$1 < $f | tee $af
+    echo " --- dif mode ---"
+    diff -y --ignore-trailing-space --ignore-blank-lines $af $of
+#    diff -y $af $of
+    if [[ $? = 1 ]] ; then
+        echo -e "\e[33mWA\e[m"
+    fi
+done
---
+++ sample_collector.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from atcodertools.client.models.contest import Contest
+from atcodertools.client.atcoder import AtCoderClient
+
+# write contest id here
+cid = ''
+
+print('Contest ID = {}'.format(cid))
+
+try:
+    at = AtCoderClient()
+    at.login()
+    cn = Contest(cid)
+    pls = at.download_problem_list(cn)
+
+except Exception as e:
+    print('ログイン/問題取得でエラー Exception = {}'.format(e))
+
+else:
+    from subprocess import run
+    from glob import glob
+
+    run(['rm', '-r', 'tests'])
+    run(['mkdir', 'tests'])
+
+    for pl in pls:
+        k = pl.alphabet
+        v = pl.get_url()
+
+        try:
+            smp = at.download_problem_content(pl)
+        except Exception as e:
+            print('{}問題で失敗 Exception = {}'.format(k, e))
+        else:
+            for i, tx in enumerate(smp.samples):
+                fn = k + str(i) + '.in'
+                f = open('tests/' + fn, 'w')
+                f.write(tx.get_input())
+                f.close()
+
+                fn = k + str(i) + '.out'
+                f = open('tests/' + fn, 'w')
+                f.write(tx.get_output())
+                f.close()
+            print('Probiem {} END'.format(k))
