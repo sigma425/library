44f17959f077c08908e60f70b058c78bfc13eaed
sigma425 <sigma425@gmail.com>
2019-10-21 14:56:21 +0900

    fix scc

--- graph/scc.cpp
+++ graph/scc.cpp
@@ -1,44 +1,49 @@
 /*
-add_edgeでグラフを作る
-cmpにnew idが入る
-0,1,2...がtopological order
+    SCC O(M+N)
+    id[v] = v の 連結成分の id
+    groups[i] : i番目の連結成分, トポロジカル順
 */
-const int MAX_V=;
-vector<int> G[MAX_V],rG[MAX_V],vs;
-bool used[MAX_V];
-int cmp[MAX_V];
-void add_edge(int from,int to){
-	G[from].push_back(to);
-	rG[to].push_back(from);
-}
-void dfs(int v){
-	used[v]=true;
-	rep(i,G[v].size()){
-		if(!used[G[v][i]]) dfs(G[v][i]);
+template <class E>
+struct SCC{
+	V<int> id;
+	VV<int> groups;
+
+	int N;
+	VV<E> G;
+	V<int> ord,low;
+	V<bool> inS;
+	V<int> st;
+    int I=0;
+
+	void dfs(int v){
+		ord[v] = low[v] = I;
+		st.pb(v); inS[v] = true;
+		for(auto e: G[v]){
+			int u = e.to;
+			if(ord[u] == -1){
+				dfs(u);
+				chmin(low[v],low[u]);
+			}else if(inS[u]){
+				chmin(low[v],ord[u]);
+			}
+		}
+		if(low[v] == ord[v]){
+			V<int> group;
+			while(true){
+				int w = st.back(); st.pop_back();
+				inS[w] = false;
+				group.pb(w);
+				if(w == v) break;
+			}
+			groups.pb(group);
+		}
 	}
-	vs.push_back(v);
-}
-void rdfs(int v,int k){
-	used[v]=true;
-	cmp[v]=k;
-	rep(i,rG[v].size()){
-		if(!used[rG[v][i]]) rdfs(rG[v][i],k);
+	SCC(const VV<E>& G_) : N(G_.size()), G(G_), ord(N,-1), low(N), inS(N){
+        id = V<int>(N);
+		rep(i,N) if(ord[i] == -1) dfs(i);
+		reverse(all(groups));
+		rep(i,groups.size()){
+			for(int v: groups[i]) id[v] = i;
+		}
 	}
-}
-vector<int> cG[MAX_V];
-int scc(int N){
-	memset(used,0,sizeof(used));
-	vs.clear();
-	rep(v,N) if(!used[v]) dfs(v);
-	memset(used,0,sizeof(used));
-	int k=0;
-	for(int i=vs.size()-1;i>=0;i--){
-		if(!used[vs[i]]) rdfs(vs[i],k++);
-	}
-	//compressed Graphを作るときはON
-	rep(i,N) for(int j:G[i]){
-		if(cmp[i]!=cmp[j]) cG[cmp[i]].pb(cmp[j]);
-	}
-	rep(i,K) sort(all(cG[i])),cG[i].erase(unique(all(cG[i])),cG[i].end());
-	return k;
-}
+};
---
+++ test_oj/scc.test.cpp
@@ -0,0 +1,57 @@
+#define PROBLEM https://judge.yosupo.jp/problem/scc
+
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(int)(n);i++)
+#define rep1(i,n) for(int i=1;i<=(int)(n);i++)
+#define all(c) c.begin(),c.end()
+#define pb push_back
+#define fs first
+#define sc second
+#define chmin(x,y) x=min(x,y)
+#define chmax(x,y) x=max(x,y)
+using namespace std;
+template<class S,class T> ostream& operator<<(ostream& o,const pair<S,T> &p){
+	return o<<"("<<p.fs<<","<<p.sc<<")";
+}
+template<class T> ostream& operator<<(ostream& o,const vector<T> &vc){
+	o<<"{";
+	for(const T& v:vc) o<<v<<",";
+	o<<"}";
+	return o;
+}
+using ll = long long;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+constexpr ll TEN(int n) { return (n == 0) ? 1 : 10 * TEN(n-1); }
+
+#ifdef LOCAL
+#define show(x) cerr << "LINE" << __LINE__ << " : " << #x << " = " << (x) << endl
+#else
+#define show(x) true
+#endif
+
+#include "../graph/scc.cpp"
+
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(false);		//DON'T USE scanf/printf/puts !!
+	cout << fixed << setprecision(20);
+	
+   int N,M;
+   cin >> N >> M;
+   struct E{int to;};
+   VV<E> G(N);
+   rep(i,M){
+       int x,y;
+       cin >> x >> y;
+       G[x].pb({y});
+   }
+   auto scc = SCC<E>(G);
+   auto& groups = scc.groups;
+   cout << groups.size() << endl;
+   for(auto g: groups){
+       cout << g.size();
+       for(int v: g) cout << " " << v;
+       cout << endl;
+   }
+}
