86929c571b5089d9365c4ef8502ce6ff66663378
sigma425 <sigma425@gmail.com>
2019-10-24 04:12:36 +0900

    add minimalcycle undirected

--- graph/MinimalCycle.cpp
+++ graph/MinimalCycle.cpp
@@ -1,11 +1,53 @@
 /*
     minimal な ( <=> induced graph も cycle, chordless?) cycle を一つ返す、なければ {}
+    Undirected / Directed
     O(V+E)
-    verified at : ABC142 F (でも、嘘(Down1個)が通ったのでまともなverifyではない・・・)
+    verified
+        Undirected : CF594 F
+        Directed : ABC142 F (でも、嘘(Down1個)が通ったのでまともなverifyではない・・・)
 
     undirected なら dfs tree の Up のうち最短を取ればおわり
     directed ならそのサイクル内で Down を使いまくる必要があって、その頂点から貪欲に最長を取ればいい
 */
+V<int> getMinimalCycleUndirected(const VV<int>& G){
+	using P = pair<int,int>;
+	int N = G.size();
+	V<int> vis(N);	//0:yet 1:now 2:done
+	V<int> par(N,-1);
+	V<int> d(N);
+
+	P U(-1,-1);	// min distance backward edge
+
+	function<void(int,int)> dfs = [&](int v,int p){
+		vis[v] = 1;
+		for(int u: G[v]) if(u != p){
+			if(vis[u] == 0){	// dfs edge
+				par[u] = v;
+				d[u] = d[v]+1;
+				dfs(u,v);
+			}else if(vis[u] == 1){	// up
+				if(U.fs == -1 || d[U.fs] - d[U.sc] > d[v] - d[u]){
+					U = P(v,u);
+				}
+			}else{}	// down
+		}
+		vis[v] = 2;
+	};
+	rep(v,N) if(vis[v] == 0){
+		dfs(v,-1);
+	}
+	if(U.fs == -1) return {};
+	V<int> c;
+	{
+		int v = U.fs;
+		while(true){
+			c.pb(v);
+			if(v == U.sc) break;
+			v = par[v];
+		}
+	}
+	return c;
+}
 
 V<int> getMinimalCycleDirected(const VV<int>& G){
 	using P = pair<int,int>;
