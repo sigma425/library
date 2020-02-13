4318db9d51c44857ebfca960a8612d2c242dfdb9
sigma425 <t.sigma425@gmail.com>
2020-02-13 11:37:32 +0900

    fix 0.cpp (per,per1,shows,mkuni...)

--- 0.cpp
+++ 0.cpp
@@ -1,13 +1,21 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for(int i=0;i<(int)(n);i++)
-#define rep1(i,n) for(int i=1;i<=(int)(n);i++)
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+#define rep(i,n) for(int i=0;i<int(n);i++)
+#define rep1(i,n) for(int i=1;i<=int(n);i++)
+#define per(i,n) for(int i=int(n)-1;i>=0;i--)
+#define per1(i,n) for(int i=int(n);i>0;i--)
 #define all(c) c.begin(),c.end()
-#define pb push_back
+#define si(x) int(x.size())
+#define pb emplace_back
 #define fs first
 #define sc second
-#define chmin(x,y) x=min(x,y)
-#define chmax(x,y) x=max(x,y)
-using namespace std;
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T,class U> void chmax(T& x, U& y){if(x<y) x=y;}
+template<class T,class U> void chmin(T& x, U& y){if(y<x) x=y;}
+template<class T> void mkuni(V<T>& v){sort(all(v));v.erase(unique(all(v)),v.end());}
 template<class S,class T> ostream& operator<<(ostream& o,const pair<S,T> &p){
 	return o<<"("<<p.fs<<","<<p.sc<<")";
 }
@@ -17,18 +25,23 @@ template<class T> ostream& operator<<(ostream& o,const vector<T> &vc){
 	o<<"}";
 	return o;
 }
-using ll = long long;
-template<class T> using V = vector<T>;
-template<class T> using VV = vector<vector<T>>;
 constexpr ll TEN(int n) { return (n == 0) ? 1 : 10 * TEN(n-1); }
 
 #ifdef LOCAL
 #define show(x) cerr << "LINE" << __LINE__ << " : " << #x << " = " << (x) << endl
+void dmpr(ostream& os){os<<endl;}
+template<class T,class... Args>
+void dmpr(ostream&os,const T&t,const Args&... args){
+	os<<t<<" ~ ";
+	dmpr(os,args...);
+}
+#define shows(...) cerr << "LINE" << __LINE__ << " : ";dmpr(cerr,##__VA_ARGS__)
 #define dump(x) cerr << "LINE" << __LINE__ << " : " << #x << " = {";  \
 	for(auto v: x) cerr << v << ","; cerr << "}" << endl;
 #else
-#define show(x) true
-#define dump(x) true
+#define show(x) void(0)
+#define dump(x) void(0)
+#define shows(...) void(0)
 #endif
 
 int main(){
@@ -36,5 +49,4 @@ int main(){
 	ios::sync_with_stdio(false);		//DON'T USE scanf/printf/puts !!
 	cout << fixed << setprecision(20);
 	
-
 }
