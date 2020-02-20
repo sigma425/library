42238c16d18f83f433139b41d1b625f4165ac0b6
sigma425 <t.sigma425@gmail.com>
2020-02-20 16:39:12 +0900

    add mincostflow_slopes

--- graph/Flow/min_cost_flow.cpp
+++ graph/Flow/min_cost_flow.cpp
@@ -72,4 +72,49 @@ struct MinCostFlow{
 		}
 		return res;
 	}
+
+	/*
+		流量を横軸に、コストを縦軸に取ったときのグラフ
+		各線分の (dx,dy/dx) の vector を返す
+		CF621 G 
+	*/
+	V<pair<C,D>> min_cost_flow_slopes(int s, int t){		// {(x,tan)}
+		V<pair<C,D>> res;
+		h = V<D>(N);
+		while(true){
+			using P = pair<D,int>;
+			priority_queue< P,vector<P>,greater<P> > que;
+			dist = V<D>(N,inf);
+			dist[s] = 0;
+			que.push(P(0,s));
+			while(!que.empty()){
+				P p = que.top();
+				que.pop();
+				int v = p.second;
+				if(dist[v] < p.first) continue;
+				for(int i=0;i<(int)G[v].size();i++){
+					edge &e = G[v][i];
+					if(e.cap>0 && dist[e.to]>dist[v]+e.cost+h[v]-h[e.to]){
+						dist[e.to]=dist[v]+e.cost+h[v]-h[e.to];
+						prevv[e.to]=v;
+						preve[e.to]=i;
+						que.push(P(dist[e.to],e.to));
+					}
+				}
+			}
+			if(dist[t]==inf) break;
+			rep(v,N) h[v]+=dist[v];
+			C f = inf;
+			for(int v=t;v!=s;v=prevv[v]){
+				chmin(f,G[prevv[v]][preve[v]].cap);
+			}
+			res.pb(f,h[t]);				// x, tan
+			for(int v=t;v!=s;v=prevv[v]){
+				edge &e=G[prevv[v]][preve[v]];
+				e.cap-=f;
+				G[v][e.rev].cap+=f;
+			}
+		}
+		return res;
+	}
 };
