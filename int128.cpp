b17e955bdea0c734783282362ab6a6510955e986
sigma425 <t.sigma425@gmail.com>
2020-04-11 16:40:08 +0900

    small fix int128

--- int128.cpp
+++ int128.cpp
@@ -1,17 +1,5 @@
-#include <bits/stdc++.h>
-#define rep(i,n) for(int i=0;i<(int)(n);i++)
-#define rep1(i,n) for(int i=1;i<=(int)(n);i++)
-#define all(c) c.begin(),c.end()
-#define pb push_back
-#define fs first
-#define sc second
-#define show(x) cout << #x << " = " << x << endl
-#define chmin(x,y) x=min(x,y)
-#define chmax(x,y) x=max(x,y)
-using namespace std;
-
-typedef __int128 ll;
-istream& operator>>(istream& i, ll& x){
+using Int = __int128;
+istream& operator>>(istream& i, Int& x){
 	x=0;
 	string s;
 	i>>s;
@@ -21,8 +9,8 @@ istream& operator>>(istream& i, ll& x){
 	if(s[0]=='-') x=-x;
 	return i;
 }
-ostream& operator<<(ostream& o, const ll& x){
-	ll tmp=x;
+ostream& operator<<(ostream& o, const Int& x){
+	Int tmp=x;
 	if(tmp==0) return o<<0;
 	if(tmp<0) o<<'-',tmp=-tmp;
 	vector<int> ds;
@@ -30,12 +18,4 @@ ostream& operator<<(ostream& o, const ll& x){
 	reverse(all(ds));
 	for(int d:ds) o<<d;
 	return o;
-}
-int main(){
-	while(true){
-		ll x;
-		cin>>x;
-		if(x==114514) break;
-		cout<<x+1<<endl;
-	}
 }
