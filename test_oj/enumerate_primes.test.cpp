738a16ffa353fcd0201cf0293b2ff6f92dd40ec6
sigma425 <t.sigma425@gmail.com>
2020-09-20 08:09:58 +0900

    add linearsieve test

---
+++ test_oj/enumerate_primes.test.cpp
@@ -0,0 +1,63 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/enumerate_primes"
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using uint = unsigned int;
+#define rep(i,n) for(int i=0;i<int(n);i++)
+#define rep1(i,n) for(int i=1;i<=int(n);i++)
+#define per(i,n) for(int i=int(n)-1;i>=0;i--)
+#define per1(i,n) for(int i=int(n);i>0;i--)
+#define all(c) c.begin(),c.end()
+#define si(x) int(x.size())
+#define pb emplace_back
+#define fs first
+#define sc second
+template<class T> using V = vector<T>;
+template<class T> using VV = vector<vector<T>>;
+template<class T,class U> void chmax(T& x, U y){if(x<y) x=y;}
+template<class T,class U> void chmin(T& x, U y){if(y<x) x=y;}
+template<class T> void mkuni(V<T>& v){sort(all(v));v.erase(unique(all(v)),v.end());}
+template<class S,class T> ostream& operator<<(ostream& o,const pair<S,T> &p){
+	return o<<"("<<p.fs<<","<<p.sc<<")";
+}
+template<class T> ostream& operator<<(ostream& o,const vector<T> &vc){
+	o<<"{";
+	for(const T& v:vc) o<<v<<",";
+	o<<"}";
+	return o;
+}
+constexpr ll TEN(int n) { return (n == 0) ? 1 : 10 * TEN(n-1); }
+
+#ifdef LOCAL
+#define show(x) cerr << "LINE" << __LINE__ << " : " << #x << " = " << (x) << endl
+void dmpr(ostream& os){os<<endl;}
+template<class T,class... Args>
+void dmpr(ostream&os,const T&t,const Args&... args){
+	os<<t<<" ~ ";
+	dmpr(os,args...);
+}
+#define shows(...) cerr << "LINE" << __LINE__ << " : ";dmpr(cerr,##__VA_ARGS__)
+#define dump(x) cerr << "LINE" << __LINE__ << " : " << #x << " = {";  \
+	for(auto v: x) cerr << v << ","; cerr << "}" << endl;
+#else
+#define show(x) void(0)
+#define dump(x) void(0)
+#define shows(...) void(0)
+#endif
+
+#include "../math/prime.cpp"
+
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(false);		//DON'T USE scanf/printf/puts !!
+	cout << fixed << setprecision(20);
+
+	int N,A,B; cin >> N >> A >> B;
+	linear_sieve(N);
+	V<int> ans;
+	for(int i=B;i<si(pr);i+=A) ans.pb(pr[i]);
+	cout << si(pr) << " " << si(ans) << endl;
+	for(int v: ans) cout << v << " ";
+	cout << endl;
+}
