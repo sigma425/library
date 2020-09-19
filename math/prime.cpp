0605fffca5649863eb62afbcbb66ed363ee98f01
sigma425 <t.sigma425@gmail.com>
2020-09-20 08:06:28 +0900

    prime -> linearsieve

--- math/prime.cpp
+++ math/prime.cpp
@@ -1,10 +1,19 @@
-typedef long long ll;
-const ll ccc=1000000;
-bool prime[ccc+1];
-vector<ll> pr;
-void makeprime(){
-	ll i,j;
-	for(i=2;i<=ccc;i++) prime[i]=true;
-	for(i=2;i*i<=ccc;i++) if(prime[i]) for(j=2;j*i<=ccc;j++) prime[j*i]=false;
-	for(i=2;i<=ccc;i++) if(prime[i]) pr.push_back(i);
+
+V<bool> isp;
+V<int> pr;
+V<int> sf; //smallest factor, sf[9*5*11] = 3
+void linear_sieve(int X){		// <= X
+	isp = V<bool>(X+1,true); isp[0] = isp[1] = false;
+	sf = V<int>(X+1);
+	for(int i=2;i<=X;i++){
+		if(isp[i]){
+			pr.pb(i);
+			sf[i] = i;
+		}
+		for(int j=0;i*pr[j]<=X;j++){
+			isp[i*pr[j]] = false;
+			sf[i*pr[j]] = pr[j];
+			if(i%pr[j] == 0) break;
+		}
+	}
 }
