2423b25dc151fb6782e898f263185693ce7b249f
sigma425 <t.sigma425@gmail.com>
2020-12-16 01:39:59 +0900

    change topo.cpp

--- graph/topo.cpp
+++ graph/topo.cpp
@@ -4,21 +4,20 @@ visを0で初期化して,Gを作って、maketopo(頂点数)すると
 topo[i]=i番目の頂点 になる
 */
 
-const int MAX_V=;
-int vis[MAX_V];		//0->yet,1->now,2->done
-vector<int> topo;
-vector<int> G[MAX_V];
-bool visit(int v){
+bool visit(int v,const vector<vector<int>>& G, vector<int>& topo, vector<int>& vis){
 	if(vis[v]==2) return 1;
 	if(vis[v]==1) return 0;
 	vis[v]=1;
-	for(int u:G[v]) if(!visit(u)) return 0;
+	for(int u:G[v]) if(!visit(u,G,topo,vis)) return 0;
 	vis[v]=2;
 	topo.pb(v);
 	return 1;
 }
-bool maketopo(int V){
-	rep(i,V) if(!visit(i)) return 0;
+vector<int> maketopo(const vector<vector<int>>& G){
+	int N = G.size();
+	vector<int> topo;
+	vector<int> vis(N);
+	rep(i,N) if(!visit(i,G,topo,vis)) return {};
 	reverse(all(topo));
-	return 1;
+	return topo;
 }
