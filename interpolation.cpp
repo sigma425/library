f71921a6f7348385932c118b2dd8aa25d4bbbc15
sigma425 <t.sigma425@gmail.com>
2021-04-19 18:47:53 +0900

    tiny interpolation with O(N^2)

--- interpolation.cpp
+++ interpolation.cpp
@@ -1,54 +1,50 @@
 /*
+	note: mod 998 で高速に多項式自体の復元をしたい時は poly.cpp を参照！！ O(Nlog^2N)
+
 	多項式補間
 	Lagrange interpolation
 	O(N^2)
 	N点(xs[i],ys[i])が与えられた時にそれを通るN-1次多項式のxでの値を返す
 	xs[i]がpairwise distinctじゃないとダメ
-	あとxs[i]==xになるときにちゃんと処理する必要がある doubleとかで誤差でxs[i]==x+epsみたいなのに注意
-
-	Z/pZとかにするなら割り算とかをちゃんと書き換えること
-
-
-	多項式自体を求めたいときは(x-xs[j])の部分が多項式の掛け算なので、全体でO(N^3)になる,
-	戻すDPっぽいのをやるとO(N^2)だが面倒そう
-	Newton補間使ったほうが良くない?
-
-	OpenCup/1101/I.cpp がmint,poly,interpolationを複合して使ってる例
 
 */
 
 template<class T>
-T interpolate(vector<T> xs,vector<T> ys,T x){
-	int N=xs.size();
+T interpolate(V<T> xs, V<T> ys, T x){
+	int N = xs.size();
+	T res = 0;
 	rep(i,N){
-		if(xs[i]==x) return ys[i];
+		T a = ys[i];
+		rep(j,N) if(j!=i) a *= (x-xs[j]) / (xs[i]-xs[j]);
+		res += a;
 	}
-	T ret=0;
-	rep(i,N){
-		T a=ys[i];
-		rep(j,N) if(j!=i) a*=(x-xs[j])/(xs[i]-xs[j]);
-		ret+=a;
-	}
-	return ret;
+	return res;
 }
 
-/*
-	多項式自体を求める O(N^3 log N)
-	O(N^2 log N) とかにしたいなら(x - x[i]) の左右からの累積積を計算しておく 面倒だから書いてないけど
-*/
-Poly<mint> interpolate(V<mint> x, V<mint> y){
-	assert(x.size() == y.size());
-	int N = x.size();
-	Poly<mint> f;
+// O(N^2)
+// verified: XXI GP of Tokyo A
+template<class T>
+V<T> interpolate(V<T> xs,V<T> ys){
+	int N = xs.size();
+	V<T> p(N+1); p[0] = 1; // \prod_i (x-xs[i])
 	rep(i,N){
-		Poly<mint> g({y[i]});
-		mint coef = 1;
-		rep(j,N) if(j!=i){
-			g *= Poly<mint>({-x[j],1});
-			coef *= (x[i]-x[j]);
+		per(j,i+1){
+			p[j+1] += p[j];
+			p[j] *= -xs[i];
 		}
-		g *= coef.inv();
-		f += g;
 	}
-	return f;
+	V<T> res(N);
+	rep(i,N){
+		// w_i := ys[i] / \prod_j(\neq i) (xs[i]-xs[j])
+		T w = 1;
+		rep(j,N) if(j!=i) w *= (xs[i]-xs[j]);
+		w = ys[i] / w;
+		// add w_i * p/(x-xs[i])
+		T v = 1;
+		per(j,N){
+			res[j] += v * w;
+			v = p[j] + v * xs[i];
+		}
+	}
+	return res;
 }
