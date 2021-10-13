4deb3082ea12f0f3c9924a737fc6b2203fd1467b
sigma425 <t.sigma425@gmail.com>
2021-10-14 02:45:36 +0900

    fix intCC (for inscribed case) + small fix

--- geom/geom.cpp
+++ geom/geom.cpp
@@ -1,22 +1,7 @@
-#include <cstdio>
-#include <iostream>
-#include <complex>
-#include <vector>
-#include <cmath>
-#include <algorithm>
-#include <cassert>
-#define rep(i,n) for(int i=0;i<n;i++)
-#define all(c) c.begin(),c.end()
-#define fs first
-#define sc second
-#define pb push_back
-#define X real()
-#define Y imag()
-using namespace std;
-typedef double D;
-typedef complex<D> P;
-typedef pair<P,P> L;
-typedef vector<P> Pol;
+using D = double;
+using P = complex<D>;
+using L = pair<P,P>;
+using Pol = vector<P>;
 struct C{P p;D r;};
 D inf=1e50,eps=1e-10,PI=acos(0.0)*2;
 bool eq(D a, D b) { return abs(a-b)<eps;}
@@ -24,10 +9,10 @@ bool eq(P a, P b) { return abs(a-b)<eps;}
 int sig(D a) { return eq(a,0) ? 0 : (a>0 ? 1 : -1);}
 int large(D a,D b){return eq(a,b)?0:(a>b?1:-1);}
 bool compxy (const P& l, const P& r){
-	return eq(l.X,r.X) ? l.Y<r.Y : l.X < r.X;
+	return eq(l.real(),r.real()) ? l.imag()<r.imag() : l.real() < r.real();
 }
 bool compyx (const P& l, const P& r){
-	return eq(l.Y,r.Y) ? l.X<r.X : l.Y < r.Y;
+	return eq(l.imag(),r.imag()) ? l.real()<r.real() : l.imag() < r.imag();
 }
 inline D dot(P a, P b) { return real(conj(a)*b);}
 inline D cro(P a, P b) { return imag(conj(a)*b);}
@@ -39,6 +24,7 @@ inline int ccw (P a, P b, P c){
 	if(eq(abs(a-c)+abs(c-b),abs(a-b))) return 0;
 	if(eq(abs(a-b)+abs(b-c),abs(a-c))) return -2;
 	if(eq(abs(c-a)+abs(a-b),abs(c-b))) return 2;
+	assert(false);
 }
 inline int ccw(L a,P p){return ccw(a.fs,a.sc,p);}
 inline P proj(P a, P b){		//ベクトルaのbへの射影
@@ -102,24 +88,25 @@ inline int iCL(C c, L l){		//num of intersection(s)
 	D d=dLP(l,c.p);
 	return eq(d,c.r) ? 1 : (d<c.r ? 2 : 0);
 }
-bool containCC(C a,C b){	//a contain c? (edge case is true)
+bool containCC(C a,C b){	//a contain b? (edge case is true)
 	return abs(a.p-b.p)+b.r<a.r+eps;
 }
+bool containCCex(C a,C b){	//a contain b? (edge case is false)
+	return abs(a.p-b.p)+b.r<a.r-eps;
+}
 // 交点が2個のとき、aをbが削ると思うと、削れた部分はret[0] -> res[1] (ccw) の部分
-inline Pol intCC(C a,C b){
-	int x=large(abs(a.p-b.p),(a.r+b.r));
-	Pol ret;
-	if(x==1) return ret;
-	if(x==0){
-		ret.pb((a.r*b.p+b.r*a.p)/(a.r+b.r));
-		return ret;
-	}
-	if(containCC(a,b)||containCC(b,a)) return ret;
-	D d=abs(a.p-b.p);
-	D theta=acos((a.r*a.r+d*d-b.r*b.r)/(2.0*a.r*d));
-	ret.pb(a.p+(b.p-a.p)/d*polar(a.r,-theta));
-	ret.pb(a.p+(b.p-a.p)/d*polar(a.r,theta));
-	return ret;
+vector<P> intCC(C a,C b){
+	if(abs(a.p-b.p) > a.r+b.r + eps) return {};
+	if(containCCex(a,b) || containCCex(b,a)) return {};
+	D d = abs(a.p-b.p);
+	D tmp = (a.r*a.r+d*d-b.r*b.r)/(2.0*a.r*d);
+	if(tmp < -1) tmp = -1;
+	if(tmp > 1) tmp = 1;
+	D theta = acos(tmp);
+	P p1 = a.p+(b.p-a.p)/d*polar(a.r,-theta);
+	P p2 = a.p+(b.p-a.p)/d*polar(a.r,theta);
+	if(eq(p1,p2)) return {p1};
+	return {p1,p2};
 }
 inline vector<L> tanCP(C c,P p){
 	int x=large(c.r,abs(p-c.p));
@@ -306,15 +293,15 @@ inline D pol_diameter(Pol p){
 inline D closest_pair(P *a, int n){
 	if(n<=1) return inf;
 	int m=n/2;
-	D x=a[m].X;
+	D x=a[m].real();
 	D d=min(closest_pair(a,m),closest_pair(a+m,n-m));
 	inplace_merge(a,a+m,a+n,compyx);
 	vector<P> b;
 	rep(i,n){
-		if(abs(x-a[i].X)>=d) continue;
+		if(abs(x-a[i].real())>=d) continue;
 		rep(j,b.size()){
 			P dp=a[i]-b[b.size()-1-j];
-			if(dp.Y>=d) break;
+			if(dp.imag()>=d) break;
 			d=min(d,abs(dp));
 		}
 		b.pb(a[i]);
