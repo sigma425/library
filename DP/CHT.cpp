d15ea00892cae5cc4d9657c0d238ab610247c09f
sigma425 <t.sigma425@gmail.com>
2024-03-26 10:57:55 +0900

    update ConvexHullTrick

--- DP/CHT.cpp
+++ DP/CHT.cpp
@@ -1,98 +1,76 @@
 /*
-	convex hull trick
-	直線集合 S={y=ax+b}に対し,次の操作ができる
-	- Sに直線を加える,ただし,「Sの中で最もaが小さいもの」を加えなきゃいけない(つまり追加クエリのaは単調減少)
-	- "与えられたx"に対し,min{a_i*x+b_i | i} を求める
+	Convex Hull Trick (追加する直線の傾きが単調なバージョン)
 
-	クエリxが単調増加の場合,incrementalにやれば全体でO(N)
-	そうでないなら,普通にSに対しにぶたんすればいい
+	初期化:
+		calc_min: true なら min, false なら max を計算
+		T: a,b,x,y などの型
+		a*b くらいの値が一箇所だけ出てくる オーバーフローに注意
+	add(a,b):
+		直線 y = ax+b を追加
+		aは広義単調増加あるいは広義単調減少 勝手に判断してくれる
+	get(x):
+		min (あるいはmax) {ax+b}
+		初期化時にtemplate引数でminかmaxか指定
+		O(log N)
+	get_incr / get_decr
+		getのクエリに単調性がある場合 amortized O(1)
+		嫌だと思うんだけど、CHT_old を使ってほしい ごめん
 
-	追加クエリが単調減少でない場合,Sをsetでもって,どの範囲を削るか頑張る必要がある
-	こっちはやることはわかるんだけど書くのが面倒 CHT2.cppを参考
-
-
-
-	↑は直線集合 にy軸に平行な直線を引いて交わる部分の最大を求める形.
-	↓は点集合に、ある傾きの直線を上からおろしていくイメージ
-
-	点集合S={(a,b)}が与えられる.次の操作ができる.
-	- Sに点を加える,ただし,「Sの中で最もaが大きいもの」を加える
-	- 与えられた傾きtに対し、その直線をどこまで下げられるか(すなわち、max b-ta)
-
-
-	この対応は、点と直線の双対((a,b) <-> y=ax-b)を取り、その後符号を反転した形になっている.(つまりy=-ax+bとすると完全にただの双対)
-
-
-	だからmaxを求める形のときは、a->-a,b->-bとしてこのコードに突っ込めば良くて、
-	注意すべきこととしては,追加クエリのaが元のaが小さい順になる
-
-
-	ていうか、どっちも凸包の上側を求めるときの途中経過がどうなってるか考えるとわかるよね?
-	下側をやろうとするとminとmaxで求められるものが変わるのと、あと追加クエリの傾きの大小関係の順番が変わる?(isrightとかもisleftに変わる気がするしちょっとこわい)
-
-
+	verify:
+		https://contest.ucup.ac/submission/364008
 */
-#include <bits/stdc++.h>
-#define rep(i,n) for(int i=0;i<(int)(n);i++)
-#define rep1(i,n) for(int i=1;i<=(int)(n);i++)
-#define all(c) c.begin(),c.end()
-#define pb push_back
-#define fs first
-#define sc second
-#define show(x) cout << #x << " = " << x << endl
-#define chmin(x,y) x=min(x,y)
-#define chmax(x,y) x=max(x,y)
-using namespace std;
-template<class S,class T> ostream& operator<<(ostream& o,const pair<S,T> &p){return o<<"("<<p.fs<<","<<p.sc<<")";}
-template<class T> ostream& operator<<(ostream& o,const vector<T> &vc){o<<"sz = "<<vc.size()<<endl<<"[";for(const T& v:vc) o<<v<<",";o<<"]";return o;}
 
-struct CHT{
-	using D = ll;
-	typedef pair<D,D> P;
-	vector<P> deq;
-	int s,sd,t;
-
-	CHT():s(0),sd(0),t(0){}
-	void add(D a,D b){		//add ax+b     a:(広義)単調減少!!!
+template<bool calc_min, typename T>
+struct ConvexHullTrick{
+	using P = pair<T,T>;
+	deque<P> q;
+	ConvexHullTrick() {}
+	void add(T a, T b){
+		if(!calc_min) a = -a, b = -b;
 		P p(a,b);
-		while(s+1<t&&check(deq[t-2],deq[t-1],p)) t--;
-		if(t == (int)deq.size()) deq.pb(p);
-		deq[t++]=p;
-	}
-	D incr_query(D x){		//x:単調増加の時,これを繰り返し呼ぶ(間にaddが挟まるのはOK)
-		while(s+1<t&&f(deq[s],x)>=f(deq[s+1],x)) s++;
-		return f(deq[s],x);
-	}
-	D decr_query(D x){		//x:単調減少の時,これを繰り返し呼ぶ(間にaddが挟まるのはOK)
-		if(sd>=t) sd=t-1;
-		while(sd+1<t&&f(deq[sd],x)>=f(deq[sd+1],x)) sd++;
-		while(sd>0&&f(deq[sd],x)<f(deq[sd-1],x)) sd--;
-		return f(deq[sd],x);
-	}
-	D query(D x){
-		int lb=s-1,ub=t-1;
-		while(ub-lb>1){
-			int m=(lb+ub)/2;
-			if(isright(deq[m],deq[m+1],x)) lb=m;
-			else ub=m;
+		if(q.empty()){
+			q.push_back(p); return;
 		}
-		return f(deq[ub],x);
+		if(q.back().fs >= a){
+			if(a == q.back().fs){
+				if(q.back().sc <= b) return;
+				q.pop_back();
+			}
+			while(si(q) >= 2 && !necessary(q[si(q)-2],q.back(),p)){
+				q.pop_back();
+			}
+			q.push_back(p);
+		}else if(a >= q.front().fs){
+			if(a == q.front().fs){
+				if(q.front().sc <= b) return;
+				q.pop_front();
+			}
+			while(si(q) >= 2 && !necessary(p,q.front(),q[1])){
+				q.pop_front();
+			}
+			q.push_front(p);
+		}else{
+			assert(false); // aが単調でない
+		}
 	}
-
-	bool isright(P& a,P& b,D x){
-		return f(a,x)>=f(b,x);
+	bool necessary(P l, P m, P r){
+		// m が最小値を取ることがあるか?
+		// assume: 傾き la > ma > ra
+		assert(l.fs > m.fs && m.fs > r.fs);
+		// (mb-lb)/(la-ma) < (rb-mb)/(ma-ra) 分母は共に正
+		// 通分すると a*b くらいの値が登場する オーバーフローに注意
+		// int128にすら入らなければ帯分数みたいにやるのもあり
+		return (m.sc-l.sc) * (m.fs-r.fs) < (r.sc-m.sc) * (l.fs-m.fs);
 	}
-	bool check(P& a,P& b,P& c){
-		return (b.fs-a.fs)*(c.sc-b.sc)>=(b.sc-a.sc)*(c.fs-b.fs);
-	}
-	D f(P &p, D x){
-		return p.fs*x+p.sc;
+	T get(T x){
+		assert(!q.empty());
+		auto f_i = [&](int i){ return q[i].fs * x + q[i].sc; };
+		int l = 0, r = si(q);
+		while(r-l > 1){
+			int m = (l+r)/2;
+			if(f_i(m-1) <= f_i(m)) r = m;
+			else l = m;
+		}
+		return f_i(l) * (calc_min ? 1 : -1);
 	}
 };
-
-int main(){
-	cht.init(3);
-	cht.add(2,3);
-	cht.add(-1,4);
-	show(cht.query(1));
-}
