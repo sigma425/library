269a878a1eb57ce8e31210f02253c67997f3751d
sigma425 <t.sigma425@gmail.com>
2024-03-26 11:08:55 +0900

    small updates

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -8,19 +8,16 @@
 	片方から片方は簡単に導ける.両方とも根付き木でかなり性質が良い.
 	計算量解析は、黒辺を下る回数が、とか思ったけど+1があるので普通にA[i]の値の遷移を考えたほうがいいか.
 
-	border列挙
+	string, vector<hoge> どっちでも使える
 
-	vector<int> res;
-	int X = kmp[N];
-	while(X>0){
-		res.pb(X);
-		X = A[X];
-	}
-	return res;
+	getBorder: [1,N] のうち borderの長さ を昇順に返す
+		特に N は常に含まれていることに注意
+
 */
 
 
-vector<int> getKMP(string s){
+template<class Str>
+vector<int> getKMP(Str s){
 	int N = s.size();
 	vector<int> A(N+1);
 	A[0] = -1;
@@ -31,4 +28,14 @@ vector<int> getKMP(string s){
 		A[i+1] = j;
 	}
 	return A;
+}
+
+template<class Str>
+V<int> getBorder(Str s){
+	int N = s.size();
+	auto kmp = getKMP(s);
+	V<int> res;
+	for(int b=N;b>0;b=kmp[b]) res.eb(b);
+	reverse(all(res));
+	return res;
 }
